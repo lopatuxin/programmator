b53984236509676579681e0895d4d60a2fdddd3c
Антон <101828305+lopatuxin@users.noreply.github.com>
2024-05-31 19:40:48 +0300

    init commit

---
+++ src/main/java/ru/programmator/ProgrammatorApplication.java
@@ -0,0 +1,13 @@
+package ru.programmator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammatorApplication.class, args);
+    }
+
+}
